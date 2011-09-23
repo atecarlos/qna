a6867028d5d50529d325f10356a706eb658c7eab
Carlos Atencio <carlos@oceangrown.com>
2011-09-22 23:56:44 -0400

    Using collection partial renderers on views. Also refactored answers_controller_specs to use only mocks and not real objects.

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -3,15 +3,30 @@ require 'spec_helper'
 describe AnswersController do
 
 	BODY = "my answer"
+    QUESTION_ID = 99
+    ANSWER_ID = 88
 
 	before(:each) do
-		@question = Fabricate(:answered_question)
-		@answer = @question.answers[0]
-		@user = Fabricate(:user)
-		sign_in @user
+		@question = mock('question')
+		@question.stub!(:id).and_return(QUESTION_ID)
+
+		@answer = mock('answer')
+		@answer.stub!(:id).and_return(ANSWER_ID)
+				
+		#for decent exposure
+		@question.stub!(:attributes=)
+		@question.stub!(:persisted?).and_return(true)
+		@answer.stub!(:attributes=)
+
+		@user = mock('user')
+
+		request.env['warden'] = mock(Warden, :authenticate => @user,
+                                             :authenticate! => @user)
 	end
 
 	it "should expose the question in context" do
+		setup_question_mock
+		
 		do_index
 		
 		controller.question.should_not be_nil
@@ -19,9 +34,12 @@ describe AnswersController do
 	end
 
 	it "should expose all answers for the question" do
+		setup_answers_mock
+
 		do_index
 
-		controller.answers.length.should == 3
+		controller.answers.length.should == 1
+		controller.answers[0].should be @answer
 	end
 
 	it "should have a new action" do
@@ -31,19 +49,24 @@ describe AnswersController do
 	end
 
 	it "should create an answer" do
-		do_create
+		setup_create_mock
+		@answer.should_receive(:save)
 
-		saved_answer = Answer.find_by_question_id_and_body(@question.id, BODY)
-		saved_answer.body.should == BODY
-		saved_answer.question.should == @question
+		do_create
 	end
 
 	it "should show all answers after after successfully adding one" do
+		setup_create_mock
+		@answer.should_receive(:save).and_return(true)
+
 		do_create
 		response.should redirect_to question_answers_path(@question)
 	end
 
 	it "should remain on new answer page if validation errors" do
+		setup_create_mock
+		@answer.should_receive(:save).and_return(false)
+
 		do_create_invalid
 
 		response.should render_template('new')
@@ -56,25 +79,59 @@ describe AnswersController do
 	end
 
 	it "should update an answer" do
+		setup_update_mock
+		
+		@answer.should_receive(:update_attributes).with({ "body" => BODY })
 		do_update
-
-		updated_answer = Answer.find(@answer.id)
-		updated_answer.body.should == BODY
 	end
 
 	it "should should show all answers after a successful update" do
+		setup_update_mock
+
+		@answer.should_receive(:update_attributes)
+			   .with({ "body" => BODY })
+			   .and_return(true)
 		do_update
 
 		response.should redirect_to question_answers_path(@question)
 	end
 
 	it "should remain in edit form if unsuccessful edit" do
+		setup_update_mock
+
+		@answer.should_receive(:update_attributes)
+			   .with({ "body" => "" })
+			   .and_return(false)
 		do_invalid_update
 
 		response.should render_template "edit"
 	end
 
 	private
+		def setup_question_mock
+			Question.should_receive(:find)
+					.with(QUESTION_ID.to_s)
+					.and_return(@question)
+		end
+
+		def setup_answers_mock
+			setup_question_mock
+			@question.should_receive(:answers)
+			     	 .and_return([ @answer ])
+		end
+
+		def setup_create_mock
+			setup_answers_mock
+			Answer.should_receive(:new).and_return(@answer)
+			@answer.should_receive(:creator=).with(@user)
+			@answer.should_receive(:question=).with(@question)
+		end
+
+		def setup_update_mock
+			setup_answers_mock
+			Answer.should_receive(:find).with(ANSWER_ID.to_s).and_return(@answer)
+		end
+
 		def do_index
 			get "index", { question_id: @question.id }
 		end
