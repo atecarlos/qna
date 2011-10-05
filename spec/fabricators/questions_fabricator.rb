699741392e5a6cc5d70b94fdec1f8203c0ceb9c6
Carlos Atencio <carlos@oceangrown.com>
2011-10-05 10:49:02 -0400

    Removed unused fabricators.

--- spec/fabricators/questions_fabricator.rb
+++ spec/fabricators/questions_fabricator.rb
@@ -3,10 +3,3 @@ Fabricator(:question) do
 	body 'How do I do this??'
 	user { Fabricate(:user) }
 end
-
-Fabricator(:answered_question, :from => :question) do
-	title { sequence(:title) { |i| "answered question #{i}" } }
-	body 'Somebody should know how to do this...right?'
-	user { Fabricate(:user) }
-	answers(count: 3) { |this_question, i| Fabricate(:answer, question: this_question, user: this_question.user) }
-end
--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -2,9 +2,3 @@ Fabricator(:user) do
 	email { sequence(:email) { |i| "user#{i}@test.com" } }
 	password 'asuper_password'
 end
-
-Fabricator(:pepe, :from => :user) do
-	email { sequence(:email) { |i| "pepe_#{i}@email.com" } }
-	password "claves"
-	questions(count: 3) { |this_user, i| Fabricate(:question, user: this_user) }
-end
