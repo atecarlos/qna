6b1952636b528c70354a02ec3b1940c95234f9f0
Carlos Atencio <carlos@oceangrown.com>
2011-11-30 07:37:50 -0500

    Refactored ability specs to be more rspec2 compliant.

--- spec/models/answer_ability_spec.rb
+++ spec/models/answer_ability_spec.rb
@@ -3,60 +3,62 @@ require 'cancan/matchers'
 
 describe Ability do
 	
-		describe "For non admins" do
+	subject { Ability.new(@user) }
+		
+	context "For non admins" do
 
 		before(:each) do
 			build_objects
 		end
 
 		it "can read answers" do
-			@ability.should be_able_to(:read, Answer)
+			should be_able_to(:read, Answer)
 		end
 
 		it "can create answers" do
-			@ability.should be_able_to(:create, Answer)
+			should be_able_to(:create, Answer)
 		end
 
 		it "can edit its own answers" do
-			@ability.should be_able_to(:edit, @answer)
+			should be_able_to(:edit, @answer)
 		end
 
 		it "can delete its own answers" do
-			@ability.should be_able_to(:destroy, @answer)
+			should be_able_to(:destroy, @answer)
 		end
 
 		it "can't edit another users answers" do
-			@ability.should_not be_able_to(:edit, @another_users_answer)
+			should_not be_able_to(:edit, @another_users_answer)
 		end
 
 		it "can't delete another users answers" do
-			@ability.should_not be_able_to(:destroy, @another_users_answer)
+			should_not be_able_to(:destroy, @another_users_answer)
 		end
 
 	end
 
-	describe "For admins" do
+	context "For admins" do
 
 		before(:each) do
 			build_objects for_admin:true
 		end
 
 		it "can read answers" do
-			@ability.should be_able_to(:read, Answer)
+			should be_able_to(:read, Answer)
 		end
 
 		it "can create answers" do
-			@ability.should be_able_to(:create, Answer)
+			should be_able_to(:create, Answer)
 		end
 
 		it "can edit any question" do
-			@ability.should be_able_to(:edit, @answer)
-			@ability.should be_able_to(:edit, @another_users_answer)
+			should be_able_to(:edit, @answer)
+			should be_able_to(:edit, @another_users_answer)
 		end
 
 		it "can delete any question" do
-			@ability.should be_able_to(:destroy, @answer)
-			@ability.should be_able_to(:destroy, @another_users_answer)
+			should be_able_to(:destroy, @answer)
+			should be_able_to(:destroy, @another_users_answer)
 		end
 
 	end
@@ -64,12 +66,11 @@ describe Ability do
 	private 
 		def build_objects(for_admin = false)
 			@user = mock_model(User, admin?:for_admin)
-			@ability = Ability.new(@user)
 
-			@answer = Answer.new(creator:@user)
+			@answer = mock_model(Answer, creator:@user)
 
 			another_user = mock_model(User)
-			@another_users_answer = Answer.new(creator:another_user)
+			@another_users_answer = mock_model(Answer, creator:another_user)
 		end
 		
 end
--- spec/models/question_ability_spec.rb
+++ spec/models/question_ability_spec.rb
@@ -3,60 +3,62 @@ require 'cancan/matchers'
 
 describe Ability do
 
-	describe "For non admins" do
+	subject { Ability.new(@user) }
+
+	context "For non admins" do
 
 		before(:each) do
 			build_objects
 		end
 
 		it "can read questions" do
-			@ability.should be_able_to(:read, Question)
+			should be_able_to(:read, Question)
 		end
 
 		it "can create questions" do
-			@ability.should be_able_to(:create, Question)
+			should be_able_to(:create, Question)
 		end
 
 		it "can edit its own questions" do
-			@ability.should be_able_to(:edit, @question)
+			should be_able_to(:edit, @question)
 		end
 
 		it "can delete its own questions" do
-			@ability.should be_able_to(:destroy, @question)
+			should be_able_to(:destroy, @question)
 		end
 
 		it "can't edit another users questions" do
-			@ability.should_not be_able_to(:edit, @another_users_question)
+			should_not be_able_to(:edit, @another_users_question)
 		end
 
 		it "can't delete another users questions" do
-			@ability.should_not be_able_to(:destroy, @another_users_question)
+			should_not be_able_to(:destroy, @another_users_question)
 		end
 
 	end
 
-	describe "For admins" do
+	context "For admins" do
 
 		before(:each) do
 			build_objects for_admin:true
 		end
 
 		it "can read questions" do
-			@ability.should be_able_to(:read, Question)
+			should be_able_to(:read, Question)
 		end
 
 		it "can create questions" do
-			@ability.should be_able_to(:create, Question)
+			should be_able_to(:create, Question)
 		end
 
 		it "can edit any question" do
-			@ability.should be_able_to(:edit, @question)
-			@ability.should be_able_to(:edit, @another_users_question)
+			should be_able_to(:edit, @question)
+			should be_able_to(:edit, @another_users_question)
 		end
 
 		it "can delete any question" do
-			@ability.should be_able_to(:destroy, @question)
-			@ability.should be_able_to(:destroy, @another_users_question)
+			should be_able_to(:destroy, @question)
+			should be_able_to(:destroy, @another_users_question)
 		end
 
 	end
@@ -65,11 +67,10 @@ describe Ability do
 
 		def build_objects(for_admin = false)
 			@user = mock_model(User, admin?:for_admin)
-			@ability = Ability.new(@user)
 
-			@question = Question.new(creator:@user)
+			@question = mock_model(Question, creator:@user)
 
 			another_user = mock_model(User)
-			@another_users_question = Question.new(creator:another_user)
+			@another_users_question = mock_model(Question, creator:another_user)
 		end	
 end
